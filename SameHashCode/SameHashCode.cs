1a6824223eb7ab242b105406068594cec4a4d2c6
Félix  Merdinger <merdinger.felix@gmail.com>
2022-09-23 13:32:57 +0200

    SameHashCode

---
+++ SameHashCode/SameHashCode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+
+public class Program
+{
+    struct ThreeStrings
+    {
+        public string   stringA;  
+        public string   stringB;
+        public string   stringC;  
+    }
+
+    static void findThreeStringsWithSameHashcode(ref ThreeStrings ret, int i = 0)
+    {
+        var firstOccurence      = new Dictionary<int, int>();
+        var secondOccurence     = new Dictionary<int, int>();
+        int currentHashcode     = 0;
+        while (true)
+        {
+            currentHashcode = i.ToString().GetHashCode();
+            try
+            {
+                firstOccurence.Add(currentHashcode,i);
+            }
+            catch (ArgumentException)
+            {
+                try
+                {
+                    secondOccurence.Add(currentHashcode,i);
+                }
+                catch (ArgumentException)
+                {
+                    ret.stringA = firstOccurence[currentHashcode].ToString();
+                    ret.stringB = secondOccurence[currentHashcode].ToString();
+                    ret.stringC = i.ToString();
+                    return;
+                }
+            }
+            i++;
+        }
+    }
+
+    static bool IsStringsDifferentHashsEqual(ThreeStrings strings)
+    {
+        return  !strings.stringA.Equals(strings.stringB) && !strings.stringB.Equals(strings.stringC)
+                && !strings.stringA.Equals(strings.stringC) && strings.stringA.GetHashCode() == strings.stringB.GetHashCode() 
+                && strings.stringB.GetHashCode() == strings.stringC.GetHashCode();
+    }
+
+    static void Main(string[] args)
+    {
+        var avg             = 0.0;
+        var worst           = 0.0;
+        var best            = 0.0;
+        int count           = 0;
+        int randomInt       = 0;
+        Random randomNumber = new Random();
+        ThreeStrings ret    = new ThreeStrings();
+
+        while(count<20)
+        {
+            randomInt = randomNumber.Next();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            findThreeStringsWithSameHashcode(ref ret,randomInt);
+            watch.Stop();
+
+            Console.WriteLine("Found: '{0}', '{1}', '{2}' in {3} ms",
+                ret.stringA, ret.stringB, ret.stringC, watch.ElapsedMilliseconds);
+            Console.WriteLine("HashCodes: '{0}', '{1}', '{2}', HashCodes equal? {3} ",
+                ret.stringA.GetHashCode(), ret.stringB.GetHashCode(), ret.stringC.GetHashCode(),IsStringsDifferentHashsEqual(ret));
+            
+            avg = (avg*count + watch.ElapsedMilliseconds ) / (count+1) ;
+            if (watch.ElapsedMilliseconds > worst)
+                worst = watch.ElapsedMilliseconds;
+            if (watch.ElapsedMilliseconds < best || best == 0.0)
+                best = watch.ElapsedMilliseconds;
+            Console.WriteLine("Average time for computing: {0} ms, best: {1} ms, worst: {2} ms, {3} iterations", avg, best, worst, count + 1);
+
+            count++;
+        }
+    }
+}
---
+++ SameHashCode/readme.md
@@ -0,0 +1,31 @@
+# Hash Code
+
+Could you please complete and return the answer to this programming question
+
+It should compile and work. You can use the internet as you would normally in your daily work life but all the code you submit should be your own.
+
+Please only send 1 .cs file with all your code.
+
+
+Write a method that will produce three (3) different strings (different string values) that have the same hash code such that:
+```
+stringA.GetHashCode() == stringB.GetHashCode() && !String.Equals(stringA, stringB) &&
+
+stringB.GetHashCode() == stringC.GetHashCode() && !String.Equals(stringB, stringC) &&
+
+!String.Equals(stringA, stringC)
+```
+
+For my solution first I did this in C# since the std::hash (corresponding to GetHashCode() in C#) returns lot more digits in C++, so collisions are more rare, and my solution would not be relevant in term of time execution versus other C# solution. It made me learn a bit of C# as well , created my first .net project wich was interesting.
+
+For me the problem was creating and incrementing strings fast to check their HashCodes.
+
+I choosed to use ints wich I incremented, and check their hashcodes with ToString(), for faster computing instead of creating a function to create and increment strings.
+
+I tried differents containers to save hashcodes, but a dictionnary of int/int was the fastest, also tried to multitread whenever I encountered duplicate hashcodes to find the 3rd occurence, but it was not faster.
+
+Since dictionnary doesn't allow more than one item of the same key and throw an exeption otherwise, I create two dictionnary for first and second occurence, and when the 2d throw an exeption, I can return the three strings directly by using the 3rd hashcode in the 1st and 2d dictionnary.
+
+I put timer and tester in the main to check performances and reliability, so I launch the function 20 times to see best,worst and average computing time, wich depends of luck factor, that's why my starting strings change everytime the function is called with use of Random().
+
+Compile with dotnet build / dotnet run.
